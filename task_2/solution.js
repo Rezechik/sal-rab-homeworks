9bbcb649ae2f9c26631941fa4a5af8a3fd851536
Rezechik <leranam.ln@gmail.com>
2023-04-08 13:07:41 +0300

    task 2 fixed

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,17 +5,12 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
     let shippingSum;
 
-if ( productsSum >= freeShippingMinSum ) {
+if ( productsSum == 0 || productsSum >= freeShippingMinSum ) {
     shippingSum = 0;
     console.log(shippingSum);
-} else
-if ( 0 < productsSum < freeShippingMinSum ) {
+} else {
     shippingSum = shippingPrice; 
     console.log(shippingSum); 
-}
-if (productsSum === 0) {
-    shippingSum = 0;
-    console.log(shippingSum);
 }
     // создайте переменную shippingSum
     // если productsSum равно 0,
@@ -35,14 +30,8 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
-    let discountSum;
-if (productsSum >= discountMinSum) {
-   discountSum = discountPart*productsSum/100;
-   console.log(discountSum);
-} else {
-    discountSum = 0;
+    let discountSum = productsSum >= discountMinSum ? discountPart * productsSum / 100 : 0;
     console.log(discountSum);
-}
     // создайте переменную discountSum
 
     // если productsSum больше или равно discountMinSum,
@@ -67,8 +56,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
     totalSum = totalSum + shippingSum;
     let freeShipping = false;
-    freeShipping = shippingSum === 0 ?
-    true : false
+    freeShipping = shippingSum == 0; 
     // прибавьте к totalSum значение shippingSum
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
